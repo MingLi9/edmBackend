77400c410d06cc7f0f74202d93dfb4b609462052
Liroy99 <mingjanssen999@gmail.com>
2023-10-21 17:35:42 +0200

    progression save
    https://youtu.be/KxqlJblhzfI?t=4890

---
+++ src/main/java/portfolio/portfolio/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package portfolio.portfolio.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import portfolio.portfolio.user.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
